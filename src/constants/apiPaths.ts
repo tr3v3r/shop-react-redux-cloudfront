a800961f5cefb3575fff6d6b1f2ddc49907c381b
Aliaksei Astafyeu <lochost40@gmail.com>
2024-10-18 12:01:59 +0200

    feat(updated): update product endpoinds

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,5 +1,6 @@
 const API_PATHS = {
-  product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  product:
+    "https://hcuwybcpt7.execute-api.us-east-1.amazonaws.com/prod/products",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
--- src/queries/products.ts
+++ src/queries/products.ts
@@ -8,9 +8,7 @@ export function useAvailableProducts() {
   return useQuery<AvailableProduct[], AxiosError>(
     "available-products",
     async () => {
-      const res = await axios.get<AvailableProduct[]>(
-        `${API_PATHS.bff}/product/available`
-      );
+      const res = await axios.get<AvailableProduct[]>(`${API_PATHS.product}`);
       return res.data;
     }
   );
@@ -29,7 +27,7 @@ export function useAvailableProduct(id?: string) {
     ["product", { id }],
     async () => {
       const res = await axios.get<AvailableProduct>(
-        `${API_PATHS.bff}/product/${id}`
+        `${API_PATHS.product}/${id}`
       );
       return res.data;
     },
